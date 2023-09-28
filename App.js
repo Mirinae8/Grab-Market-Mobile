ee0616d267b3a147b173d610eb7ec226a9185302
Mirinae8 <gdjw816@naver.com>
2023-09-28 15:27:07 +0900

    판매상품 블러처리, 캐러셀 배너 추가

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  Alert,
+  SafeAreaView,
+} from "react-native";
 import BasketballImage from "./assets/products/basketball1.jpeg";
 import Avatar from "./assets/icons/avatar.png";
 import { API_URL } from "./config/constatns";
@@ -9,12 +19,14 @@ import React from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
+import Carousel from "react-native-reanimated-carousel";
 
 dayjs.extend(relativeTime);
 dayjs.locale("ko");
 
 export default function App() {
   const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   // useEffect는 첫번째 콜백함수의 실행을 관리할 수 있다
   React.useEffect(() => {
     axios
@@ -26,53 +38,98 @@ export default function App() {
       .catch((error) => {
         console.error(error);
       });
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        setBanners(result.data.banners);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
   return (
-    // 웹은 자동 스크롤을 지원하지만 모바일 환경에서는 리액트 네이티브의 ScrollView를 사용해야 한다
-    <View style={styles.container}>
-      <ScrollView>
-        <Text style={styles.headLine}>판매되는 상품들</Text>
-        <View style={styles.productList}>
-          {products.map((product, index) => {
-            return (
-              <View style={styles.productCard}>
-                {/* 이미지 영역 */}
-                <View>
-                  {/* 리액트 네이티브는 리엑트와 달리 이미지를 import 해야 한다*/}
-                  {/* 리액트 에서는 object-fit 를 사용해 이미지 해상도 비율을 유지했지만 리액트 네이티브는 resizeMode 사용 */}
+    // 상단 노치부분을 비우기 위해 사용하는 컴포넌트 (주로 최상단에 작성)
+    <SafeAreaView style={styles.safeAreaView}>
+      {/* // 웹은 자동 스크롤을 지원하지만 모바일 환경에서는 리액트 네이티브의 ScrollView를 사용해야 한다 */}
+      <View style={styles.container}>
+        <ScrollView>
+          <Carousel
+            // {/* 캐러셀에 넣을 필수 데이터 */}
+            data={banners}
+            width={Dimensions.get("window").width}
+            height={200}
+            autoPlay={true}
+            // {/* Dimensions 객체는 디바이스의 너비와 높이를 확인할 수 있는 객체 (슬라이더 길이를 화면 너비만큼)*/}
+            sliderWidth={Dimensions.get("window").width}
+            itemWidth={Dimensions.get("window").width}
+            itemHeight={200}
+            // {/* obj에 들어가는 값은 data에 있는 데이터를 넣어줌 (배열이므로 각각의 객체를 넣음) */}
+            // 함수형태로 데이터를 넣어줘야 한다 (맵처럼 각각 반환해야함)
+            renderItem={(obj) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    Alert.alert("배너 클릭");
+                  }}
+                >
+                  {/* <Text>{`${API_URL}/${obj.item.imageUrl}`}</Text> */}
                   <Image
-                    style={styles.productImage}
-                    //   source={BasketballImage} 내부 이미지 불러온것
-                    // 외부 이미지를 가져오기 위해서는
+                    style={styles.bannerImage}
                     source={{
-                      // 해당 이미지 주소를 넣는다
-                      uri: `${API_URL}/${product.imageUrl}`,
+                      uri: `${API_URL}/${obj.item.imageUrl}`,
                     }}
-                    resizeMode={"contain"}
+                    resizeMode="contain"
                   />
-                </View>
-                {/* 컨텐츠 영역 */}
-                <View style={styles.prodcutContents}>
-                  <Text style={styles.productName}>{product.name}</Text>
-                  <Text style={styles.prodcutPrice}>{product.price}원</Text>
-                  <View style={styles.productFooter}>
-                    <View style={styles.productSeller}>
-                      <Image style={styles.productAvatar} source={Avatar} />
-                      <Text style={styles.productSellerName}>
-                        {product.seller}
+                </TouchableOpacity>
+              );
+            }}
+          />
+          <Text style={styles.headLine}>판매되는 상품들</Text>
+          <View style={styles.productList}>
+            {/* 맵으로 매번 다른 리턴하는 부분에서 리액트에서 랜더링시 성능 향상을 위해 키값을 넣어줘야함 */}
+            {products.map((product, index) => {
+              return (
+                <View style={styles.productCard} key={index}>
+                  {/* 상품이 품절된 경우에만 블러처리 and 조건문으로 앞을 통과해야 뒤가 동작하도록*/}
+                  {product.soldout === 1 && <View style={styles.prodcutBlur} />}
+                  {/* 이미지 영역 */}
+                  <View>
+                    {/* 리액트 네이티브는 리엑트와 달리 이미지를 import 해야 한다*/}
+                    {/* 리액트 에서는 object-fit 를 사용해 이미지 해상도 비율을 유지했지만 리액트 네이티브는 resizeMode 사용 */}
+                    <Image
+                      style={styles.productImage}
+                      //   source={BasketballImage} 내부 이미지 불러온것
+                      // 외부 이미지를 가져오기 위해서는
+                      source={{
+                        // 해당 이미지 주소를 넣는다
+                        uri: `${API_URL}/${product.imageUrl}`,
+                      }}
+                      resizeMode={"contain"}
+                    />
+                  </View>
+                  {/* 컨텐츠 영역 */}
+                  <View style={styles.prodcutContents}>
+                    <Text style={styles.productName}>{product.name}</Text>
+                    <Text style={styles.prodcutPrice}>{product.price}원</Text>
+                    <View style={styles.productFooter}>
+                      <View style={styles.productSeller}>
+                        <Image style={styles.productAvatar} source={Avatar} />
+                        <Text style={styles.productSellerName}>
+                          {product.seller}
+                        </Text>
+                      </View>
+                      <Text style={styles.productDate}>
+                        {dayjs(product.createdAt).fromNow()}
                       </Text>
                     </View>
-                    <Text style={styles.productDate}>
-                      {dayjs(product.createdAt).fromNow()}
-                    </Text>
                   </View>
                 </View>
-              </View>
-            );
-          })}
-        </View>
-      </ScrollView>
-    </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -136,5 +193,24 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "800",
     marginBottom: 24,
+  },
+  prodcutBlur: {
+    // 포지션은 부모기준으로해서 자신의 위치 결정 (다른 자식과 상관없이)
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: "#ffffffaa",
+    // 제트 인덱스는 깊이( 높을수록 더 위로 사용자가 보이도록)
+    zIndex: 999,
+  },
+  bannerImage: {
+    width: "100%",
+    height: 200,
+  },
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: "#fff",
   },
 });
--- config/constatns.js
+++ config/constatns.js
@@ -1,3 +1,3 @@
 // ngrok 으로 로컬의 expres 서버를 외부에서 접근 가능하도록 했다
 // ngrok 을 킬때마다 주소가 바뀌므로 매번 새로운 주소를 넣어줘야 한다
-export const API_URL = "https://aa85-121-164-72-244.ngrok-free.app";
+export const API_URL = "https://211a-121-164-72-244.ngrok-free.app";
