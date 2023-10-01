d58628f0a34ab120d47933904bf35cbc07f2057a
Mirinae8 <gdjw816@naver.com>
2023-10-01 15:13:15 +0900

    상품 상세 페이지 구매 버튼 구현

--- config/constatns.js
+++ config/constatns.js
@@ -1,3 +1,4 @@
 // ngrok 으로 로컬의 expres 서버를 외부에서 접근 가능하도록 했다
 // ngrok 을 킬때마다 주소가 바뀌므로 매번 새로운 주소를 넣어줘야 한다
-export const API_URL = "https://934f-121-164-72-244.ngrok-free.app";
+// 로컬 서버에서 fly.io 서버로 변경
+export const API_URL = "https://grab-market-server-gdjw.fly.dev";
--- screens/product.js
+++ screens/product.js
@@ -8,9 +8,12 @@ import {
   ActivityIndicator,
   StyleSheet,
   ScrollView,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
 import { API_URL } from "../config/constatns";
 import Avatar from "../assets/icons/avatar.png";
+import dayjs from "dayjs";
 
 export default function ProductScreen(props) {
   // 프롭스에서 화면이동시 넣어준 값 가져옴
@@ -27,6 +30,13 @@ export default function ProductScreen(props) {
         console.error(error);
       });
   }, []);
+
+  const onPressButton = () => {
+    if (!product.soldout) {
+      Alert.alert("구매가 완료되었습니다");
+    }
+  };
+
   // product는 처음에 null이므로 방어 코드 작성
   if (!product) {
     // 리액트 네이티브에서 제공하는 로딩 아이콘
@@ -48,8 +58,29 @@ export default function ProductScreen(props) {
             <Text>{product.seller}</Text>
           </View>
           <View style={styles.divider} />
+          <View>
+            <Text style={styles.productName}>{product.name}</Text>
+            <Text style={styles.productPrice}>{product.price}원</Text>
+            <Text style={styles.productDate}>
+              {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+            </Text>
+            <Text style={styles.productDescription}>{product.description}</Text>
+          </View>
         </View>
       </ScrollView>
+      {/* 플로팅 버튼 화면 스크롤해도 위치 고정된 버튼 */}
+      <TouchableOpacity onPress={onPressButton}>
+        {/* 판매된 상품에는 다른 스타일을 적용 */}
+        <View
+          style={
+            product.soldout ? styles.purchaseDisabled : styles.purchaseButton
+          }
+        >
+          <Text style={styles.purchaseText}>
+            {product.soldout === 1 ? "구매완료" : "구매하기"}
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -85,5 +116,54 @@ const styles = StyleSheet.create({
     height: 1,
     // 마진 탑,바탐을 한번에 사용하는 리액트 네이티브 기본 속성
     marginVertical: 16,
+  },
+
+  productName: {
+    fontSize: 20,
+    fontWeight: "400",
+  },
+
+  productPrice: {
+    fontSize: 18,
+    fontWeight: "700",
+    marginTop: 8,
+  },
+
+  productDate: {
+    fontSize: 14,
+    marginTop: 4,
+    color: "rgb(204,204,204)",
+  },
+
+  productDescription: {
+    marginTop: 16,
+    fontSize: 17,
+  },
+
+  purchaseButton: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 60,
+    backgroundColor: "rgb(255,80,88)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  purchaseText: {
+    color: "white",
+    fontSize: 20,
+  },
+
+  purchaseDisabled: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "gray",
   },
 });
